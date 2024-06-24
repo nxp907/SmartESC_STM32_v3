ba37bbff8a91ccc2be7b917c0f4f9c0ea34c692d
nxp907 <61973892+nxp907@users.noreply.github.com>
2024-06-24 19:37:14 +0200

    Update config.h

--- Core/Inc/config.h
+++ Core/Inc/config.h
@@ -5,7 +5,7 @@
  *  Author: stancecoke
  */
 
-#ifndef CONFIG_H_
+#define CONFIG_H_
 #define CONFIG_H_
 #include "stdint.h"
 
@@ -31,43 +31,43 @@
 #define CAL_I 38		// ADC counts * CAL_I = current in mA
 
 // gains for PI controls
-#define P_FACTOR_I_Q 100
+#define P_FACTOR_I_Q 200
 #define I_FACTOR_I_Q 2
-#define P_FACTOR_I_D 100
+#define P_FACTOR_I_D 200
 #define I_FACTOR_I_D 10
 
 // min and max values of throttle and brake signals in ADC counts
 #define THROTTLEOFFSET 45
-#define THROTTLEMAX 175
+#define THROTTLEMAX 200
 #define BRAKEOFFSET 50
 #define BRAKEMAX 190
 
 // parameters for speed calculation
 #define WHEEL_CIRCUMFERENCE 550 //690 for original M365 motor
-#define GEAR_RATIO 11 //15 for original M365 motor
+#define GEAR_RATIO 15 //15 for original M365 motor
 
 // speed limits for invividual modes in kph
-#define SPEEDLIMIT_ECO 6
-#define SPEEDLIMIT_NORMAL 20
-#define SPEEDLIMIT_SPORT 50
+#define SPEEDLIMIT_ECO 25
+#define SPEEDLIMIT_NORMAL 55
+#define SPEEDLIMIT_SPORT 55
 
 // motor current limits for invividual modes in mA, see default settings at https://max.cfw.sh/#
-#define PH_CURRENT_MAX_ECO 16000
-#define PH_CURRENT_MAX_NORMAL 28000
+#define PH_CURRENT_MAX_ECO 26000
+#define PH_CURRENT_MAX_NORMAL 55000
 #define PH_CURRENT_MAX_SPORT 55000
 
 // motor current limit for regen in mA
 #define REGEN_CURRENT 20000
 
 // maximum current for flux weakening in mA
-#define FW_CURRENT_MAX 18000 //max id
+#define FW_CURRENT_MAX 55000 //max id
 
 // maximum battery currents in mA
-#define BATTERYCURRENT_MAX 14500
-#define REGEN_CURRENT_MAX 10000
+#define BATTERYCURRENT_MAX 30000
+#define REGEN_CURRENT_MAX 20000
 
 // battery voltage limits in mV
-#define BATTERYVOLTAGE_MIN 33000
+#define BATTERYVOLTAGE_MIN 32000
 #define BATTERYVOLTAGE_MAX 42000
 
 
@@ -76,7 +76,7 @@
 
 // settings for speed PLL (angle estimation)
 #define SPEED_PLL 1 //1 for using PLL, 0 for angle extrapolation
-#define P_FACTOR_PLL 10 //7 for original M365 motor
-#define I_FACTOR_PLL 10 //7 for original M365 motor
+#define P_FACTOR_PLL 7 //7 for original M365 motor
+#define I_FACTOR_PLL 7 //7 for original M365 motor
 
 #endif /* CONFIG_H_ */
