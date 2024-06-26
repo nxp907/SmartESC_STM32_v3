fd419239dac8fb56fc30ea0405c48ff96ee74bed
nxp907 <61973892+nxp907@users.noreply.github.com>
2024-06-26 14:36:01 +0200

    Update config.h

--- Core/Inc/config.h
+++ Core/Inc/config.h
@@ -1,24 +1,24 @@
 #define THROTTLEOFFSET 45
-#define THROTTLEMAX 175
+#define THROTTLEMAX 200
 #define BRAKEOFFSET 50
 #define BRAKEMAX 100
 
 // speed limits for invividual modes in kph
-#define SPEEDLIMIT_ECO 6
-#define SPEEDLIMIT_NORMAL 20
-#define SPEEDLIMIT_SPORT 50
+#define SPEEDLIMIT_ECO 25
+#define SPEEDLIMIT_NORMAL 200
+#define SPEEDLIMIT_SPORT 200
 
 // motor current limits for invividual modes in mA
 // note that hacked firmware allows up to 55amps motor phase current
-#define PH_CURRENT_MAX_ECO 10000
-#define PH_CURRENT_MAX_NORMAL 15000
-#define PH_CURRENT_MAX_SPORT 25000
+#define PH_CURRENT_MAX_ECO 15000
+#define PH_CURRENT_MAX_NORMAL 30000
+#define PH_CURRENT_MAX_SPORT 30000
 
 // motor current limit for regen in mA
-#define REGEN_MAX_CURRENT 10000
+#define REGEN_MAX_CURRENT 15000
 
 // maximum current for field weakening in mA
-#define FIELD_WEAKNING_CURRENT_MAX 0 //max id
+#define FIELD_WEAKNING_CURRENT_MAX 20000 //max id
 
 // ADC channels
 #define ADC_VOLTAGE 0
